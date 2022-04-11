c9f6b79fc2cbbf1a72ffc6190996fcfe34259609
reload1bronze <nature9th@gmail.com>
2022-04-11 15:49:21 +0900

    예외 처리(404), post 삭제시 comments 삭제 - DB에서 다큐먼트 못찾으면 404페이지 렌더링 - post 삭제할 경우 연관 comments 모두 삭제

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,92 +15,159 @@ router.get("/posts", (req, res) => {
 router.post("/posts", async (req, res) => {
   const { title, author, contents } = req.body;
 
-  await Post.create({
-    title,
-    author,
-    contents,
-  });
-
-  return res.redirect("/");
+  try {
+    await Post.create({
+      title,
+      author,
+      contents,
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.status(400).render("writePage", {
+      errorMessage: error.message,
+    });
+  }
 });
 
-router.get("/posts/:id", async (req, res) => {
+router.get("/posts/:id([0-9a-f]{24})", async (req, res) => {
   const { id } = req.params;
   const post = await Post.findById(id).populate("comments");
+
+  if (!post) {
+    return res.status(404).render("404Page");
+  }
 
   return res.render("detailPage", { post });
 });
 
-router.get("/posts/:id/edit", async (req, res) => {
+router.get("/posts/:id([0-9a-f]{24})/edit", async (req, res) => {
   const { id } = req.params;
   const post = await Post.findById(id);
+
+  if (!post) {
+    return res.status(404).render("404Page");
+  }
+
   return res.render("editPage", { post });
 });
 
-router.post("/posts/:id/edit", async (req, res) => {
+router.post("/posts/:id([0-9a-f]{24})/edit", async (req, res) => {
   const { title, author, contents } = req.body;
   const { id } = req.params;
+
+  const post = await Post.exists({ _id: id });
+  if (!post) {
+    return res.status(404).render("404Page");
+  }
+
   await Post.findByIdAndUpdate(id, { $set: { title, author, contents } });
   return res.redirect(`/posts/${id}`);
 });
 
-router.get("/posts/:id/delete", async (req, res) => {
+router.get("/posts/:id([0-9a-f]{24})/delete", async (req, res) => {
   const { id } = req.params;
+
+  const post = await Post.findById(id).populate("comments");
+  if (!post) {
+    return res.status(404).render("404Page");
+  }
+
+  // post와 연관된 comments도 DB에서 삭제
+  const comments = post.comments;
+  for (const comment of comments) {
+    await Comment.findByIdAndDelete(comment._id);
+  }
+
+  console.log(comments);
+
   await Post.findByIdAndDelete(id);
   return res.redirect("/");
 });
 
-router.post("/posts/:id/comments", async (req, res) => {
+router.post("/posts/:id([0-9a-f]{24})/comments", async (req, res) => {
   const { id } = req.params;
   const { commenter, comment } = req.body;
-  const newComment = await Comment.create({
-    commenter,
-    comment,
-  });
 
-  const post = await Post.findById(id);
-  post.comments.push(newComment._id);
-  post.save();
-
-  return res.redirect(`/posts/${id}`);
-});
-
-router.get("/posts/:id/:commentId/delete", async (req, res) => {
-  const { id, commentId } = req.params;
-  const comment = await Comment.findById(commentId);
-
-  const post = await Post.findById(id);
-  post.comments.pull(comment._id);
-  post.save();
-
-  await Comment.findByIdAndDelete(commentId);
-
-  return res.redirect(`/posts/${id}`);
-});
-
-router.get("/posts/:id/:commentId/edit", async (req, res) => {
-  console.log("댓글 수정 페이지");
-  const { id, commentId } = req.params;
-  const comment = await Comment.findById(commentId);
-  const post = await Post.findById(id).populate("comments");
-  console.log(comment);
-  console.log(post);
-
-  return res.render("detailPage", { post, comment });
-});
-
-router.post("/posts/:id/:commentId/edit", async (req, res) => {
-  const { id, commentId } = req.params;
-  const { commenter, comment } = req.body;
-
-  const delComment = await Comment.findByIdAndUpdate(commentId, {
-    $set: {
+  try {
+    const newComment = await Comment.create({
       commenter,
       comment,
-    },
-  });
+    });
 
-  return res.redirect(`/posts/${id}`);
+    const post = await Post.findById(id);
+    post.comments.push(newComment._id);
+    post.save();
+
+    return res.redirect(`/posts/${id}`);
+  } catch (error) {
+    return res.status(400).render(`/posts/${id}`, {
+      errorMessage: error.message,
+    });
+  }
 });
+
+router.get(
+  "/posts/:id([0-9a-f]{24})/:commentId([0-9a-f]{24})/delete",
+  async (req, res) => {
+    const { id, commentId } = req.params;
+
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      return res.status(404).render("404Page");
+    }
+
+    const post = await Post.findById(id);
+    if (!post) {
+      return res.status(404).render("404Page");
+    }
+    post.comments.pull(comment._id);
+    post.save();
+
+    await Comment.findByIdAndDelete(commentId);
+
+    return res.redirect(`/posts/${id}`);
+  }
+);
+
+router.get(
+  "/posts/:id([0-9a-f]{24})/:commentId([0-9a-f]{24})/edit",
+  async (req, res) => {
+    const { id, commentId } = req.params;
+
+    const comment = await Comment.findById(commentId);
+    if (!comment) {
+      return res.status(404).render("404Page");
+    }
+
+    const post = await Post.findById(id).populate("comments");
+    if (!post) {
+      return res.status(404).render("404Page");
+    }
+
+    return res.render("detailPage", { post, comment });
+  }
+);
+
+router.post(
+  "/posts/:id([0-9a-f]{24})/:commentId([0-9a-f]{24})/edit",
+  async (req, res) => {
+    const { id, commentId } = req.params;
+    const { commenter, comment } = req.body;
+
+    const delComment = await Comment.exists({ _id: commentId });
+    if (!delComment) {
+      return res.status(404).render("404Page");
+    }
+
+    await Comment.findByIdAndUpdate(commentId, {
+      $set: {
+        commenter,
+        comment,
+      },
+    });
+
+    return res.redirect(`/posts/${id}`);
+  }
+);
 
 module.exports = router;
